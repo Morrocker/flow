7d50886ae9c5abd77b3e3e2ad46582ac124983e9
Ignacio Romero <morocco@Morocco-MacBook-Air-M1.local>
2021-05-26 11:48:44 -0400

    basic functions done

---
+++ controller.go
@@ -0,0 +1,46 @@
+package flow
+
+import "github.com/morrocker/broadcast"
+
+// type Gate struct{
+
+// }
+type Controller struct {
+	broadcaster *broadcast.Broadcaster
+	allow       bool
+	exitVal     int
+}
+
+func New() *Controller {
+	c := &Controller{
+		broadcaster: broadcast.New(),
+	}
+	return c
+}
+
+func (c *Controller) Checkpoint() int {
+	l := c.broadcaster.Listen()
+	for {
+		if c.allow {
+			l.Close()
+			return c.exitVal
+		}
+		<-l.C
+	}
+}
+
+func (c *Controller) Stop() {
+	c.allow = false
+}
+
+func (c *Controller) Go() {
+	c.allow = true
+	c.exitVal = 0
+	c.broadcaster.Broadcast()
+}
+
+func (c *Controller) Exit(n int) {
+	c.allow = true
+	c.exitVal = n
+	c.broadcaster.Broadcast()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/morrocker/flow-controller
+
+go 1.16
+
+require github.com/morrocker/broadcast v0.0.0-20210331144645-71f2ee18ca39
